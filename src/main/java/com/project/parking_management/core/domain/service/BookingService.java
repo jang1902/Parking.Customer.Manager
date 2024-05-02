5f1bcc917bcf274cf82290a12a729e829bddd566
Nguyễn Trung Kiên <KienNT@vissoft.vn>
2024-05-02 11:16:18 +0700

    change package location

--- src/main/java/com/project/parking_management/core/domain/service/BookingService.java
+++
@@ -1,4 +0,0 @@
-package com.project.parking_management.core.domain.service;
-
-public interface BookingService {
-}
--- src/main/java/com/project/parking_management/core/domain/service/impl/BookingServiceImpl.java
+++
@@ -1,4 +0,0 @@
-package com.project.parking_management.core.domain.service.impl;
-
-public class BookingServiceImpl {
-}
