f24cbd53add013442a5b92ea3408cb53dc6ba2c8
m0wxddm <ducduong.16699@gmail.com>
2024-05-04 08:51:58 +0700

    update all

---
+++ src/main/java/com/project/parking_management/infrastructure/controller/CustomerController.java
@@ -0,0 +1,8 @@
+package com.project.parking_management.infrastructure.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class CustomerController {
+}
