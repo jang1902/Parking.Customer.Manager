bdce204950dfc2e15e4f2115693d2a05d102b2ba
Jang <jang1902er@gmail.com>
2024-05-04 17:05:23 +0700

    Add function getCurrentLocation, modify getNearestLocation (remove 2 param)

--- src/main/java/com/project/parking_management/core/service/NearestTransitService.java
+++ src/main/java/com/project/parking_management/core/service/NearestTransitService.java
@@ -1,7 +1,11 @@
 package com.project.parking_management.core.service;
 
+import com.google.maps.model.DistanceMatrix;
+import com.google.maps.model.LatLng;
 import com.project.parking_management.core.domain.dto.response.NearestTransitResponse;
 
 public interface NearestTransitService {
-    NearestTransitResponse getNearestLocation(Double latitude, Double longitude);
+    public NearestTransitResponse getNearestLocation();
+
+    public LatLng GetCurrentCoordinate();
 }
--- src/main/java/com/project/parking_management/core/service/impl/NearestTransitServiceImpl.java
+++ src/main/java/com/project/parking_management/core/service/impl/NearestTransitServiceImpl.java
@@ -2,15 +2,21 @@ package com.project.parking_management.core.service.impl;
 
 import com.google.maps.DistanceMatrixApi;
 import com.google.maps.GeoApiContext;
+
 import com.google.maps.model.DistanceMatrix;
 import com.google.maps.model.LatLng;
+import com.project.parking_management.core.config.GeoLocationConfig;
 import com.project.parking_management.core.config.GoogleApiConfig;
+import com.project.parking_management.core.domain.dto.response.GeoLocationResponse;
 import com.project.parking_management.core.domain.dto.response.NearestTransitResponse;
 import com.project.parking_management.core.domain.entity.ParkingLot;
 import com.project.parking_management.core.service.NearestTransitService;
 import com.project.parking_management.core.service.ParkingLotService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,26 +24,30 @@ import java.util.List;
 @Service
 public class NearestTransitServiceImpl implements NearestTransitService {
 
+
     private final GeoApiContext context;
     private final ParkingLotService parkingLotService;
+    private final GeoLocationConfig geoLocationConfig;
 
     @Autowired
-    public NearestTransitServiceImpl(GoogleApiConfig config , ParkingLotService parkingLotService) {
+    public NearestTransitServiceImpl(GoogleApiConfig config , ParkingLotService parkingLotService, GeoLocationConfig geoLocationConfig) {
         context=new GeoApiContext.Builder().apiKey(config.getApiKey()).build();
         this.parkingLotService=parkingLotService;
+        this.geoLocationConfig=geoLocationConfig;
     }
 
     @Override
-    public NearestTransitResponse getNearestLocation(Double latitude, Double longitude) {
+    public NearestTransitResponse getNearestLocation() {
         List<ParkingLot> listParkingLot;
         List<DistanceMatrix> distanceMatrixList =new ArrayList<>();
         NearestTransitResponse response=new NearestTransitResponse();
+        LatLng currentCoordinates = this.GetCurrentCoordinate();
 
         listParkingLot=parkingLotService.getAllParkingLots();
         try {
             for(ParkingLot parkingLot:listParkingLot){
                 DistanceMatrix distanceMatrix= DistanceMatrixApi.newRequest(context)
-                        .origins(new LatLng(latitude,longitude))
+                        .origins(currentCoordinates)
                         .destinations(new LatLng(parkingLot.getLatitude(),parkingLot.getLongitude())).await();
                 distanceMatrixList.add(distanceMatrix);
             }
@@ -65,8 +75,21 @@ public class NearestTransitServiceImpl implements NearestTransitService {
                 min=distanceMatrix.rows[0].elements[0].distance.inMeters;
             }
         }
+
         return objectMin;
     }
 
+    @Override
+    public LatLng GetCurrentCoordinate(){
+        LatLng currentCoordinates = new LatLng();
+        String geoLocationUrl= geoLocationConfig.getUrl();
 
+        RestTemplate restTemplate = new RestTemplate();
+        GeoLocationResponse geoLocationResponse = restTemplate.getForObject(geoLocationUrl, GeoLocationResponse.class);
+        if(geoLocationResponse!=null) {
+            currentCoordinates.lat=geoLocationResponse.getLatitude();
+            currentCoordinates.lng=geoLocationResponse.getLongitude();
+        }
+        return currentCoordinates;
+    }
 }
